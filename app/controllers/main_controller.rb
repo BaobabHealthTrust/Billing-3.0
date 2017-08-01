9b07bc7cc15d6e6bf53eaf91e55b54f343e0c2b2
timmtonga <timmtonga@gmail.com>
2017-08-01 11:56:04 +0200

    Fixed main dashboard details

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,10 @@
 class MainController < ApplicationController
   def index
-    range = DateTime.current.beginning_of_day..DateTime.current.end_of_day
-    @collected = OrderPayment.select("COALESCE(sum(amount),0) as amount").where(payment_stamp: range).first.amount rescue 0
+    range = DateTime.now.beginning_of_day..DateTime.now.end_of_day
+    @collected = OrderPayment.select("COALESCE(sum(amount),0) as amount").where(created_at: range).first.amount rescue 0
     @billed = OrderEntry.select("COALESCE(sum(full_price),0) as amount").where(order_date: range).first.amount rescue 0
     @registrations = Patient.select("COALESCE(count(*),0) as number").where(date_created: range).first.number rescue 0
-    @cash_payments = OrderPayment.select("COALESCE(count(*),0) as number").where(payment_stamp: range, payment_mode: "CASH").first.number rescue 0
+    @cash_payments = Receipt.select("COALESCE(count(*),0) as number").where(payment_stamp: range, payment_mode: "CASH").first.number rescue 0
     @pending = @billed - @collected
 
   end
