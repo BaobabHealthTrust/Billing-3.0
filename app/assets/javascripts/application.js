26b08dec421f735bfb9c22eeb2bd9318b15f69a5
timmtonga <timmtonga@gmail.com>
2017-08-04 01:39:07 +0200

    Added flash notice display to application layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -147,3 +147,69 @@ function press(pressedChar){
     search.value+= pressedChar
     search_box.fnFilter(search.value)
 }
+function getBrowserHeight() {
+    var intH = 0;
+    var intW = 0;
+
+    if(typeof window.innerWidth  == 'number' ) {
+        intH = window.innerHeight;
+        intW = window.innerWidth;
+    }
+    else if(document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+        intH = document.documentElement.clientHeight;
+        intW = document.documentElement.clientWidth;
+    }
+    else if(document.body && (document.body.clientWidth || document.body.clientHeight)) {
+        intH = document.body.clientHeight;
+        intW = document.body.clientWidth;
+    }
+
+    return { width: parseInt(intW), height: parseInt(intH) };
+}
+
+function setLayerPosition(background, main) {
+    var shadow = document.getElementById(background);
+    var question = document.getElementById(main);
+
+    var bws = getBrowserHeight();
+    shadow.style.width = bws.width + "px";
+    shadow.style.height = bws.height + "px";
+
+    question.style.left = parseInt((bws.width - 350) / 11);
+    question.style.top = parseInt((bws.height - 200) / 15);
+
+    shadow = null;
+    question = null;
+}
+
+function showLayer(background, main) {
+    setLayerPosition(background, main);
+
+    var shadow = document.getElementById(background);
+    var question = document.getElementById(main);
+
+    shadow.style.display = "block";
+    question.style.display = "block";
+
+    shadow = null;
+    question = null;
+}
+
+function hideLayer(background,main) {
+    var shadow = document.getElementById(background);
+    var question = document.getElementById(main);
+
+    shadow.style.display = "none";
+    question.style.display = "none";
+
+    shadow = null;
+    question = null;
+}
+
+function confirmAction(actionUrl, message)
+{
+    showLayer('shadow', 'question')
+    document.getElementById("bttnYes").setAttribute("onmousedown", "window.location='"+actionUrl + "'")
+    document.getElementById("message").innerHTML = message;
+}
+
