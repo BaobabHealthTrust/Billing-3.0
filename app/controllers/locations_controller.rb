a2868412db1999f3d69453e5fd75590c84e73f0b
timmtonga <timmtonga@gmail.com>
2017-08-06 10:37:30 +0200

    Improved locaation printing code

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -58,7 +58,10 @@ class LocationsController < ApplicationController
 
   def search
 
-    names = Location.select(:name, :location_id).where("name LIKE '%#{params[:search_string]}%'").map do |v|
+    workstation_id = LocationTag.select(:location_tag_id).find_by_name("workstation location")
+    locations = workstation_id.location_tag_map.collect{|x| x.location_id}
+    names = Location.select(:name, :location_id).where("name LIKE '%#{params[:search_string]}%'
+                                                        AND location_id in (?)", locations).map do |v|
       "<li value=\"#{v.location_id}\">#{v.name}</li>"
     end
 
