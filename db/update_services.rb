4045c74a6ca3e741cf5c8245e4389e68be721fac
timmtonga <timmtonga@gmail.com>
2017-09-02 09:10:12 -0400

    Finalized services seed

--- db/update_services.rb
+++ db/update_services.rb
@@ -14,24 +14,35 @@ CSV.foreach("#{Rails.root}/db/updated_list_final.csv",{:headers=>:first_row, :co
   type = ServiceType.where(name: row[1]).first.id rescue nil
   next if type.blank?
   service = Service.where({name: row[0], service_type_id: type}).first_or_initialize
+  service.unit = row[4].strip
+  service.rank = (row[5].to_i)
   service.creator = creator.id
   service.save
 
-  if !row[2].blank? || row[2] == '0'
-    service_price = ServicePrice.where(service_id: service.id,price_type: 'General').first_or_initialize
-    service_price.price = (row[2].blank? ? 0.00 : row[2].to_f)
-    service_price.creator = creator.id
-    service_price.updated_by = creator.id
-    service_price.save
+  if row[6] == 'TRUE'
+    service.voided = true
+    service.voided_by = creator.id
+    service.voided_reason = "Duplicate"
+    service.voided_date = Date.current
+    service.save
+  else
+    if !row[2].blank? || row[2] == '0'
+      service_price = ServicePrice.where(service_id: service.id,price_type: 'General').first_or_initialize
+      service_price.price = (row[2].blank? ? 0.00 : row[2].to_f)
+      service_price.creator = creator.id
+      service_price.updated_by = creator.id
+      service_price.save
+    end
+
+    if !row[3].blank? || row[3] == '0'
+      service_price = ServicePrice.where(service_id: service.id,price_type: 'Private').first_or_initialize
+      service_price.price = (row[3].blank? ? 0.00 : row[3].to_f)
+      service_price.creator = creator.id
+      service_price.updated_by = creator.id
+      service_price.save
+    end
   end
 
-  if !row[3].blank? || row[3] == '0'
-    service_price = ServicePrice.where(service_id: service.id,price_type: 'Private').first_or_initialize
-    service_price.price = (row[3].blank? ? 0.00 : row[3].to_f)
-    service_price.creator = creator.id
-    service_price.updated_by = creator.id
-    service_price.save
-  end
 end
 
 puts 'Loading service panels'
